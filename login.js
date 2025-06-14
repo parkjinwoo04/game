771a6bded31490ff3f60a16c413d9a794f08bdeb
parkjinwoo04 <133664212+parkjinwoo04@users.noreply.github.com>
2025-06-14 17:09:03 +0900

    Create login.js

---
+++ login.js
@@ -0,0 +1,26 @@
+const loginForm = document.getElementById("login_form");
+const logintInput = document.querySelectot("#login_form input");
+const logintButton = document.querySelectot("#login_form button");
+
+function onLofintBtnClick() {
+  const playerName = loginInput.value;
+  if (playerName === "") {
+    alert("이름을 입력하세요");
+  } else if (playerName.length < 8) {
+    alert(`너무 깁니다. (최대 8글자)`);
+  } else {
+    console.log (loginInput.value;  
+  }
+}
+
+loginButton.addEventListener ("click", onLoginBtnClick);
+
+
+
+
+
+
+
+
+
+
